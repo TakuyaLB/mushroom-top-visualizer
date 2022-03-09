39fecb731c95519b5101a353661982e5a493569b
Chaitu Nookala <chaitanya.nookala@gmail.com>
2022-03-09 20:41:24 +0000

    combine rect_positions, text_objects, and list_nums into one list using the dataclass Number

--- boxes.py
+++ boxes.py
@@ -1,4 +1,5 @@
 import pygame
+from dataclasses import dataclass
 import random
 
 pygame.init()
@@ -9,12 +10,17 @@ win = pygame.display.set_mode((1300, 700))
 pygame.display.set_caption("Boxes")
 
 
+@dataclass
+class Number:
+    num: int
+    rect: pygame.Rect
+    text: pygame.Surface
+
+
 class Animation:
     def __init__(self, list_nums):
-        self.list_nums = list_nums
-        self.size = len(self.list_nums)
-        self.rect_positions = {}
-        self.text_objects = {}
+        self.nums = []
+        self.size = len(list_nums)
         self.width, self.height = 1300, 700
         self.space = 10
         self.box_size = int(min((self.width - self.space * (self.size + 1)) / self.size, 60))
@@ -22,53 +28,51 @@ class Animation:
         self.left_border = (self.width - ((self.box_size * self.size) + (self.size - 1) * self.space)) // 2
         self.top_border = (self.height - self.box_size) // 2
         for i, n in enumerate(list_nums):
-            self.rect_positions[n] = pygame.Rect(self.left_border + (self.box_size + self.space) * i, self.top_border, self.box_size, self.box_size)
+            rect = pygame.Rect(self.left_border + (self.box_size + self.space) * i, self.top_border, self.box_size, self.box_size)
             text = self.font.render(str(list_nums[i]), True, 'white')
             text_rect = text.get_rect()
             text_rect.center = (self.left_border + (self.box_size + self.space) * i + self.box_size // 2), (self.top_border + self.box_size // 2)
-            self.text_objects[list_nums[i]] = text
+            self.nums.append(Number(n, rect, text))
 
     def draw_boxes(self, win):
         win.fill((0, 0, 0))
-        for i, n in enumerate(self.list_nums):
-            pygame.draw.rect(win, 'red', self.rect_positions[n])
-            text = self.text_objects[n]
+        for i, n in enumerate(self.nums):
+            rect = n.rect
+            pygame.draw.rect(win, 'red', rect)
+            text = n.text
             text_rect = text.get_rect()
-            text_rect.center = self.rect_positions[n].center
+            text_rect.center = rect.center
             win.blit(text, text_rect)
         pygame.display.update()
 
     def swap(self, index1, index2):
         if index1 == index2:
             return
-        list_nums = self.list_nums
 
         dist = abs(index1 - index2) - 1
-        num1 = self.list_nums[index1]
-        num2 = self.list_nums[index2]
         if index1 < index2:
-            rect1 = self.rect_positions[num1]
-            rect2 = self.rect_positions[num2]
+            rect1 = self.nums[index1].rect
+            rect2 = self.nums[index2].rect
         else:
-            rect1 = self.rect_positions[num2]
-            rect2 = self.rect_positions[num1]
+            rect1 = self.nums[index2].rect
+            rect2 = self.nums[index1].rect
 
 
         step = 10
         dist_to_target = self.box_size * 1.5
         ori_y = rect1.y
 
-        while (rect1.y <= ori_y + dist_to_target):
+        while rect1.y <= ori_y + dist_to_target:
             rect1.y += step
             rect2.y -= step
             self.draw_boxes(win)
             pygame.time.delay(50)
 
         ori_x = rect1.x
-        target_x1 = ((dist + 1) * (self.box_size) + (dist + 1) * (self.space))
+        target_x1 = ((dist + 1) * self.box_size + (dist + 1) * self.space)
         ori_x2 = rect2.x
 
-        while (rect1.x < ori_x + target_x1 - step):
+        while rect1.x < ori_x + target_x1 - step:
             rect1.x += step
             rect2.x -= step
             self.draw_boxes(win)
@@ -79,42 +83,43 @@ class Animation:
 
         ori_y = rect1.y
 
-        while (rect1.y >= ori_y - dist_to_target):
+        while rect1.y >= ori_y - dist_to_target:
             rect1.y -= step
             rect2.y += step
             self.draw_boxes(win)
             pygame.time.delay(50)
 
-        num1_idx = self.list_nums.index(num1)
-        num2_idx = self.list_nums.index(num2)
-        tmp = self.list_nums[num1_idx]
-        self.list_nums[num1_idx] = self.list_nums[num2_idx]
-        self.list_nums[num2_idx] = tmp
+        tmp = self.nums[index1]
+        self.nums[index1] = self.nums[index2]
+        self.nums[index2] = tmp
 
     def bubble_sort(self):
         is_sorted = False
         while not is_sorted:
             is_sorted = True
-            for i in range(len(self.list_nums[:-1])):
-                if int(self.list_nums[i]) > int(self.list_nums[i+1]):
+            for i in range(len(self.nums[:-1])):
+                if self.nums[i].num > self.nums[i+1].num:
                     self.swap(i, i+1)
                     is_sorted = False
 
 
 run = True
 
-animation = Animation(list(range(10, 0, -1)))
+# unsorted = [random.randint(0, 100) for i in range(10)]
+unsorted = [1, 2, 3, 4, 5, 6, 5, 7, 8]
+print(unsorted)
+animation = Animation(unsorted)
 animation.draw_boxes(win)
 animation.bubble_sort()
 
 
-# while run:
-#     pygame.time.delay(10)
-#
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             run = False
-#
-#     animation.draw_boxes(win)
-#
-# pygame.quit()
+while run:
+    pygame.time.delay(10)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    animation.draw_boxes(win)
+
+pygame.quit()
